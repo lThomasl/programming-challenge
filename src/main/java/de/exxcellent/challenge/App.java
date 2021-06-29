121067c9ce79211c76dab87dfd70eee5c6a2d596
Thomas <thomas.mayer92@gmx.net>
2021-06-29 15:19:56 +0200

    Revert "add main test for the weather and football challenge"
    This reverts commit fda9a084626edda32885759d3a4dcb53d1041793.

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -4,16 +4,15 @@ package de.exxcellent.challenge;
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
  *
- * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>, Thomas Mayer
+ * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 public final class App {
 
     /**
      * This is the main entry method of your program.
      * @param args The CLI arguments passed
-     * @return 
      */
-    public static String main(String... args) {
+    public static void main(String... args) {
 
         // Your preparation code …
 
@@ -22,6 +21,5 @@ public final class App {
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
-        return "";
     }
 }
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,28 +1,31 @@
 package de.exxcellent.challenge;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Example JUnit 5 test case.
- * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>, Thomas Mayer
+ * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
 
-    @Test
-    void runFootball() {
-    	String correctTeam = "Team with smallest goal spread       : Aston_Villa";
-    	String calculatedTeam = App.main("--football", "football.csv");
-    	 assertEquals(correctTeam , calculatedTeam, "The main  method do not return the Team with the lowest goal distance of the footballcsv file.");
-    }
-    
-    @Test
-    void runWeather() {
-    	String beatifullDay = "Day with smallest temperature spread : 14";
-    	String calculatedDay = App.main("--weatherDistance", "weather.csv");
-        assertEquals(beatifullDay , calculatedDay, "The main  method do not return the day with the lowest temperature distance of the weather.csv file.");
+    private String successLabel = "not successful";
+
+    @BeforeEach
+    void setUp() {
+        successLabel = "successful";
     }
 
+    @Test
+    void aPointlessTest() {
+        assertEquals("successful", successLabel, "My expectations were not met");
+    }
+
+    @Test
+    void runFootball() {
+        App.main("--football", "football.csv");
+    }
 
 }
